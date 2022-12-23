2758dba6d24a33b1a53d6def84b061d8992aa4aa
kjc6735 <penguin_03@naver.com>
2022-12-24 04:59:21 +0900

    flutter todo app example

---
+++ lib/controller/TodoController.dart
@@ -0,0 +1,19 @@
+
+import 'package:get/get.dart';
+import 'package:my_todo/model/Todo.dart';
+
+class TodoController extends GetxController{
+  var todos = <Todo>[].obs;
+  int index = 1;
+  void addTodo(String text){
+    todos.add(Todo(id:index++, text:text, done:false));
+  }
+  void deleteTodo(index){
+    todos.removeWhere((element) => element.id == index);
+  }
+
+  void toggleTodo(index){
+    todos[index] = Todo(id:todos[index].id, text:todos[index].text, done: !todos[index].done);
+    //todos.indexWhere((element) => false)
+  }
+}
---
+++ lib/model/Todo.dart
@@ -0,0 +1,12 @@
+class Todo{
+  int id;
+  String text;
+  bool done;
+
+  Todo({
+    required this.id,
+    required this.text,
+    required this.done
+  });
+
+}
